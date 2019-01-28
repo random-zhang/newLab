6014b672288f4fc8f55050e3e422673755d50db7
m1998cc <2586814380@qq.com>
2019-01-28 23:35:57 +0800

    更改了若干操作

--- app/src/main/java/lab/ourteam/newlab/devices_activity.java
+++ app/src/main/java/lab/ourteam/newlab/devices_activity.java
@@ -2,12 +2,26 @@ package lab.ourteam.newlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class devices_activity extends AppCompatActivity {
+import lab.ourteam.newlab.activity.Iactivity;
+
+public class devices_activity extends AppCompatActivity implements Iactivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.devices_activity);
+
+    }
+
+    @Override
+    public void initID() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
